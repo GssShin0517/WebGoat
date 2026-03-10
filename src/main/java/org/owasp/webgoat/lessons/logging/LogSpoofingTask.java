f66e4fdd2e1d86917cfd9a73b06f4150fc7b9d64
GSS_DOMAIN\shin_yen <shin_yen@gss.com.tw>
2026-03-10 12:00:14 +0800

    fix: xss potential issues

--- src/main/java/org/owasp/webgoat/lessons/logging/LogSpoofingTask.java
+++ src/main/java/org/owasp/webgoat/lessons/logging/LogSpoofingTask.java
@@ -7,6 +7,7 @@ package org.owasp.webgoat.lessons.logging;
 import static org.owasp.webgoat.container.assignments.AttackResultBuilder.failed;
 import static org.owasp.webgoat.container.assignments.AttackResultBuilder.success;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.logging.log4j.util.Strings;
 import org.owasp.webgoat.container.assignments.AssignmentEndpoint;
 import org.owasp.webgoat.container.assignments.AttackResult;
@@ -22,15 +23,16 @@ public class LogSpoofingTask implements AssignmentEndpoint {
   @ResponseBody
   public AttackResult completed(@RequestParam String username, @RequestParam String password) {
     if (Strings.isEmpty(username)) {
-      return failed(this).output(username).build();
+      return failed(this).output(StringEscapeUtils.escapeHtml4(username)).build();
+      username = StringEscapeUtils.escapeHtml4(username);
     }
     username = username.replace("\n", "<br/>");
     if (username.contains("<p>") || username.contains("<div>")) {
       return failed(this).output("Try to think of something simple ").build();
     }
     if (username.indexOf("<br/>") < username.indexOf("admin")) {
-      return success(this).output(username).build();
+      return success(this).output(StringEscapeUtils.escapeHtml4(username)).build();
     }
-    return failed(this).output(username).build();
+    return failed(this).output(StringEscapeUtils.escapeHtml4(username)).build();
   }
 }
